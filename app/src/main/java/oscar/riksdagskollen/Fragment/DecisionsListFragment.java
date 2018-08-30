519f46d40a3f83861005655515122361417aa0a4
Oscar Andell <oscar@andell.eu>
2018-08-30 14:48:38 +0200

    Change gradle versioncode

--- app/src/main/java/oscar/riksdagskollen/Fragment/DecisionsListFragment.java
+++ app/src/main/java/oscar/riksdagskollen/Fragment/DecisionsListFragment.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.android.volley.VolleyError;
 
 import java.util.ArrayList;
