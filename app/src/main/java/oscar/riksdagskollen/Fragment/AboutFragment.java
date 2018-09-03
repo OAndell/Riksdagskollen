30b2fff87db2335619f651fe62baaa7007bfc5c1
Gustav Aaro <gustav.aaro@gmail.com>
2018-09-03 18:46:50 +0200

    Add notices of open source software

--- app/src/main/java/oscar/riksdagskollen/Fragment/AboutFragment.java
+++ app/src/main/java/oscar/riksdagskollen/Fragment/AboutFragment.java
@@ -11,7 +11,13 @@ import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.franmontiel.attributionpresenter.AttributionPresenter;
+import com.franmontiel.attributionpresenter.entities.Attribution;
+import com.franmontiel.attributionpresenter.entities.Library;
+import com.franmontiel.attributionpresenter.entities.License;
 
 import oscar.riksdagskollen.R;
 
@@ -20,10 +26,13 @@ import oscar.riksdagskollen.R;
  */
 
 public class AboutFragment extends Fragment {
+
+    private Button licenceButton;
     public static AboutFragment newInstance(){
         AboutFragment newInstance = new AboutFragment();
         return newInstance;
     }
+
 
     @Nullable
     @Override
@@ -46,5 +55,45 @@ public class AboutFragment extends Fragment {
             }
         });
 
+        final AttributionPresenter attributionPresenter = new AttributionPresenter.Builder(getContext())
+                .addAttributions(
+                        Library.GSON)
+                .addAttributions(
+                        new Attribution.Builder("jsoup: Java HTML Parser")
+                                .addCopyrightNotice("Copyright Jonathan Hedley 2009 - 2017")
+                                .addLicense(License.MIT)
+                                .setWebsite("https://jsoup.org/")
+                                .build(),
+                        new Attribution.Builder("Volley")
+                                .addLicense(License.APACHE)
+                                .addCopyrightNotice("Copyright 2011 Google")
+                                .setWebsite("https://github.com/google/volley")
+                                .build(),
+                        new Attribution.Builder("FlexboxLayout")
+                                .addCopyrightNotice("Copyright 2016 Google")
+                                .addLicense(License.APACHE)
+                                .setWebsite("https://github.com/google/flexbox-layout")
+                                .build(),
+                        new Attribution.Builder("MPAndroidChart")
+                                .addCopyrightNotice("Copyright 2018 Philipp Jahoda")
+                                .addLicense(License.APACHE)
+                                .setWebsite("https://github.com/PhilJay/MPAndroidChart")
+                                .build(),
+                        new Attribution.Builder("AttributionPresenter")
+                                .addCopyrightNotice("Copyright 2017 Francisco José Montiel Navarro")
+                                .addLicense(License.APACHE)
+                                .setWebsite("https://github.com/franmontiel/AttributionPresenter")
+                                .build()
+                )
+                .build();
+
+
+        licenceButton = view.findViewById(R.id.show_licenses_button);
+        licenceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attributionPresenter.showDialog("Programvara som hjälpt till att skapa Riksdagskollen");
+            }
+        });
     }
 }
