2c64e5fed7f9fabf0f3d41eaf046e54d587ebe8d
Gustav Aaro <gustav.aaro@gmail.com>
2018-09-19 20:14:50 +0200

    Add local unit test class and a test for representative sourceid parsing

---
+++ app/src/androidTest/java/oscar/riksdagskollen/UnitTests.java
@@ -0,0 +1,27 @@
+package oscar.riksdagskollen;
+
+import org.junit.Test;
+
+import oscar.riksdagskollen.Util.JSONModel.Representative;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by gustavaaro on 2018-09-19.
+ */
+public class UnitTests {
+
+    @Test
+    public void sourceIdParsingFromImgUrl() {
+        assertThat(Representative.getSourceIdFromImageUrl(
+                "https://data.riksdagen.se/filarkiv/bilder/ledamot/ac737989-5fa0-44bc-ad69-c1a0ddba71bb_320.jpg")
+                .equals("ac737989-5fa0-44bc-ad69-c1a0ddba71bb"), is(true));
+        assertThat(Representative.getSourceIdFromImageUrl(
+                "http://data.riksdagen.se/filarkiv/bilder/ledamot/727e2213-ac97-4ceb-bfab-f228debd8074_80.jpg")
+                .equals("727e2213-ac97-4ceb-bfab-f228debd8074"), is(true));
+    }
+
+}
+
+
