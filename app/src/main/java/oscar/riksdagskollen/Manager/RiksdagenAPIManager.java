d484b398ed91f7a9b9e446fac456faea9329702a
OAndell <Oscar@Andell.eu>
2018-12-20 18:28:06 +0100

    Git test

--- app/src/main/java/oscar/riksdagskollen/Manager/RiksdagenAPIManager.java
+++ app/src/main/java/oscar/riksdagskollen/Manager/RiksdagenAPIManager.java
@@ -639,7 +639,7 @@ public class RiksdagenAPIManager {
 
             @Override
             public void onFail(VolleyError error) {
-
+                callback.onFail(error);
             }
 
             /*@Override
