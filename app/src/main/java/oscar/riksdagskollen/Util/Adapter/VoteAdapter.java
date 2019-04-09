144a17b31c0f026f7c9e6969cefacc31f1e4d2d5
OAndell <Oscar@Andell.eu>
2019-04-09 21:23:27 +0800

    Add chat bubble drawable

--- app/src/main/java/oscar/riksdagskollen/Util/Adapter/VoteAdapter.java
+++ app/src/main/java/oscar/riksdagskollen/Util/Adapter/VoteAdapter.java
@@ -90,7 +90,6 @@ public class VoteAdapter extends RiksdagenViewHolderAdapter {
         super(listener);
         setSortedList(voteList);
         addAll(items);
-
         this.clickListener = listener;
 
     }
