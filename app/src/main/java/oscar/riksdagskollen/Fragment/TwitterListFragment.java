dc28bc44e29f80afc446184d3696a70376d86d58
OAndell <Oscar@Andell.eu>
2019-06-15 16:39:33 +0800

    Add basic first version of tweetadapter and twitter fragment

---
+++ app/src/main/java/oscar/riksdagskollen/Fragment/TwitterListFragment.java
@@ -0,0 +1,89 @@
+package oscar.riksdagskollen.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.VolleyError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import oscar.riksdagskollen.R;
+import oscar.riksdagskollen.Util.Adapter.RiksdagenViewHolderAdapter;
+import oscar.riksdagskollen.Util.Adapter.TweetAdapter;
+import oscar.riksdagskollen.Util.Enum.CurrentParties;
+import oscar.riksdagskollen.Util.Helper.TwitterUserFactory;
+import oscar.riksdagskollen.Util.JSONModel.Twitter.Tweet;
+import oscar.riksdagskollen.Util.JSONModel.Twitter.TwitterUser;
+import oscar.riksdagskollen.Util.RiksdagenCallback.TwitterCallback;
+
+
+public class TwitterListFragment extends RiksdagenAutoLoadingListFragment {
+    private final List<Tweet> documentList = new ArrayList<>();
+    private TweetAdapter adapter;
+
+    private TwitterUser twitterUser = TwitterUserFactory.getUser(CurrentParties.getV());
+
+    public static TwitterListFragment newInstance() {
+        TwitterListFragment newInstance = new TwitterListFragment();
+        return newInstance;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.debate_nav);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new TweetAdapter(documentList, new RiksdagenViewHolderAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Object clickedDocument) {
+
+            }
+        });
+    }
+
+    /**
+     * Load the next page and add it to the adapter when downloaded and parsed.
+     * Hides the loading view.
+     */
+    protected void loadNextPage() {
+        setLoadingMoreItems(true);
+        twitterUser.getTimeline(new TwitterCallback() {
+            @Override
+            public void onTweetsFetched(List<Tweet> tweets) {
+                documentList.addAll(tweets);
+                getAdapter().addAll(documentList);
+                setShowLoadingView(false);
+                setLoadingMoreItems(false);
+            }
+
+            @Override
+            public void onFail(VolleyError error) {
+
+            }
+        });
+
+        incrementPage();
+    }
+
+    @Override
+    protected void clearItems() {
+        documentList.clear();
+        adapter.clear();
+    }
+
+    @Override
+    RiksdagenViewHolderAdapter getAdapter() {
+        return adapter;
+    }
+}
---
+++ app/src/main/java/oscar/riksdagskollen/Util/Adapter/TweetAdapter.java
@@ -0,0 +1,166 @@
+package oscar.riksdagskollen.Util.Adapter;
+
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+
+import oscar.riksdagskollen.R;
+import oscar.riksdagskollen.RiksdagskollenApp;
+import oscar.riksdagskollen.Util.JSONModel.Twitter.Tweet;
+import oscar.riksdagskollen.Util.View.CircularImageView;
+
+public class TweetAdapter extends RiksdagenViewHolderAdapter {
+
+    private static final Comparator<Tweet> DEFAULT_COMPARATOR = new Comparator<Tweet>() {
+        @Override
+        public int compare(Tweet a, Tweet b) {
+            return 0;
+        }
+    };
+    private Comparator<Tweet> mComparator = DEFAULT_COMPARATOR;
+    private final SortedList<Tweet> tweets = new SortedList<>(Tweet.class, new SortedList.Callback<Tweet>() {
+        @Override
+        public int compare(Tweet o1, Tweet o2) {
+            return mComparator.compare(o1, o2);
+        }
+
+        @Override
+        public void onChanged(int position, int count) {
+            notifyItemRangeChanged(position, count);
+        }
+
+        @Override
+        public boolean areContentsTheSame(Tweet oldItem, Tweet newItem) {
+            return false;
+        }
+
+        @Override
+        public boolean areItemsTheSame(Tweet item1, Tweet item2) {
+            return false;
+        }
+
+
+        @Override
+        public void onInserted(int position, int count) {
+            notifyItemRangeInserted(position, count);
+        }
+
+        @Override
+        public void onRemoved(int position, int count) {
+            notifyItemRangeRemoved(position, count);
+        }
+
+        @Override
+        public void onMoved(int fromPosition, int toPosition) {
+            notifyItemMoved(fromPosition, toPosition);
+        }
+    });
+
+
+    public TweetAdapter(List<Tweet> tweetsList, OnItemClickListener clickListener) {
+        super(clickListener);
+        addAll(tweetsList);
+        setSortedList(tweets);
+
+
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.tweet_item, parent, false);
+            return new TweetAdapter.TweetViewHolder(itemView);
+        } else {
+            FrameLayout frameLayout = new FrameLayout(parent.getContext());
+            //make sure it fills the space
+            frameLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            return new HeaderFooterViewHolder(frameLayout);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (position < headers.size()) {
+            View v = headers.get(position);
+            //add our view to a header view and display it
+            prepareHeaderFooter((HeaderFooterViewHolder) holder, v);
+        } else if (position >= headers.size() + tweets.size()) {
+            View v = footers.get(position - tweets.size() - headers.size());
+            //add our view to a footer view and display it
+            prepareHeaderFooter((HeaderFooterViewHolder) holder, v);
+        } else {
+            Tweet document = tweets.get(position);
+            ((TweetAdapter.TweetViewHolder) holder).bind(document, this.clickListener);
+        }
+    }
+
+
+    @Override
+    public void addAll(List<?> items) {
+        tweets.addAll((Collection<Tweet>) items);
+    }
+
+    @Override
+    public void removeAll(List<?> items) {
+        tweets.beginBatchedUpdates();
+        for (Object item : items) {
+            tweets.remove((Tweet) item);
+        }
+        tweets.endBatchedUpdates();
+    }
+
+    @Override
+    public void replaceAll(List<?> items) {
+        tweets.beginBatchedUpdates();
+        tweets.clear();
+        tweets.addAll((Collection<Tweet>) items);
+        tweets.endBatchedUpdates();
+    }
+
+
+    public static class TweetViewHolder extends RecyclerView.ViewHolder {
+        private final TextView tweetText;
+        private final TextView date;
+        private final NetworkImageView image;
+        private final CircularImageView authorView;
+
+
+        public TweetViewHolder(View textView) {
+            super(textView);
+            tweetText = textView.findViewById(R.id.tweet_text);
+            date = textView.findViewById(R.id.publicerad);
+            image = textView.findViewById(R.id.image);
+            authorView = textView.findViewById(R.id.author_img);
+
+        }
+
+        public void bind(final Tweet tweet, final OnItemClickListener listener) {
+            //TODO : if retweet do stuff
+            tweetText.setText(tweet.getText());
+            date.setText(tweet.getCreated_at());
+            if (tweet.hasMedia()) {
+                image.setVisibility(View.VISIBLE);
+                image.setDefaultImageResId(R.drawable.ic_placeholder_image_web);
+                //TODO : create a get image function in Tweet Class.
+                image.setImageUrl(tweet.getEntities().getMedia()[0].getMedia_url_https(),
+                        RiksdagskollenApp.getInstance().getRequestManager().getmImageLoader());
+            } else {
+                image.setVisibility(View.GONE);
+            }
+
+            //TODO : authorView image.
+        }
+    }
+}
