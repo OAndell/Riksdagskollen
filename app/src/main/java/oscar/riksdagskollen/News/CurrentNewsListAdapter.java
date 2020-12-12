d886373eb956c644772d4f7bdb66c4c1d2eff5ad
Johannes <johannes_semere@hotmail.com>
2020-12-12 13:48:40 +0100

    remove unused import

--- app/src/main/java/oscar/riksdagskollen/News/CurrentNewsListAdapter.java
+++ app/src/main/java/oscar/riksdagskollen/News/CurrentNewsListAdapter.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
